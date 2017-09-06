b1ac29d7f1d9c3216f844a89b595740cb0d660c3
twshli <shli@thoughtworks.com>
2017-09-06 20:02:47 +0800

    Correct id generation strategy

--- src/main/java/pipeline/entity/Student.java
+++ src/main/java/pipeline/entity/Student.java
@@ -2,6 +2,7 @@ package pipeline.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -11,7 +12,7 @@ import javax.persistence.Id;
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String sid;
 
     private String name;
